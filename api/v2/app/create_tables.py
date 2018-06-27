f2634f1e02274f0b5b997f7223c4119a9ae735a1
Quantum-35 <quantumcomputing35@icloud.com>
2018-06-27 09:20:33 +0300

    Fix failing build on travis

--- api/v2/app/create_tables.py
+++ api/v2/app/create_tables.py
@@ -1,4 +1,5 @@
 import psycopg2
+from flask import current_app
 
 from app.app import create_app
 
@@ -8,10 +9,11 @@ This module creates tables for all the users and rides
 
 def create_tables():
     # Connection to the database
-    if create_app('development'):
-        conn  = psycopg2.connect(host="localhost",database="andela", user="postgres", password="leah")
-    else:
+    if current_app.config['TESTING']:
         conn  = psycopg2.connect(host="localhost",database="test_rides", user="foo", password="bar")
+            
+    else:
+        conn  = psycopg2.connect(host="localhost",database="andela", user="postgres", password="leah")
     curs = conn.cursor()
 
     tbl_users = (
