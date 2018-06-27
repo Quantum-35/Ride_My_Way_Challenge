29ae78f05c9d6eb9b8dc955e7d8525c47219bebb
Quantum-35 <quantumcomputing35@icloud.com>
2018-06-27 22:44:45 +0300

    [Feature #158660783] write a failing test for the endpoint user can view all ride offers

--- api/v2/tests/test_rides.py
+++ api/v2/tests/test_rides.py
@@ -13,7 +13,6 @@ class TestRides(BaseTests):
         response = self.login_user()
         self.assertTrue(response.status_code == 200)
         access_token = json.loads(response.data)['token']
-        print(access_token)
         headers = dict(Authorization='Bearer {}'.format(access_token))
         response = self.client.post(RIDES_URL,
                                     data=json.dumps(self.test_ride_data),
@@ -23,5 +22,19 @@ class TestRides(BaseTests):
         self.assertTrue(response.status_code == 201)
         expected = {'message': 'Ride Successfully Created'}
         self.assertEquals(expected['message'], json.loads(response.data)['message'])
+
+    def test_user_can_get_all_available_rides(self):
+        response = self.register_user()
+        self.assertTrue(response.status_code == 201)
+        response = self.login_user()
+        self.assertTrue(response.status_code == 200)
+        access_token = json.loads(response.data)['token']
+        headers = dict(Authorization='Bearer {}'.format(access_token))
+        response = self.client.get(RIDES_URL,
+                                   content_type='application/json',
+                                   headers=headers)
+        self.assertTrue(response.status_code == 200)
+        expected = {'status': 'ok'}
+        self.assertTrue(expected['status'], json.loads(response.data)['status'])
 
     
