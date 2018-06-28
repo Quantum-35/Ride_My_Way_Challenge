5d18d5a1dd711422179c1cec5ca467a55e9ed046
Quantum-35 <quantumcomputing35@icloud.com>
2018-06-28 06:02:23 +0300

    [Feature #158675260]write a failing tests for the endpoint where logged in user can view details of a single ride

--- api/v2/tests/test_rides.py
+++ api/v2/tests/test_rides.py
@@ -37,4 +37,28 @@ class TestRides(BaseTests):
         expected = {'status': 'ok'}
         self.assertTrue(expected['status'], json.loads(response.data)['status'])
 
+    def test_user_can_view_details_ofan_offer(self):
+        response = self.register_user()
+        self.assertTrue(response.status_code == 201)
+        response = self.login_user()
+        self.assertTrue(response.status_code == 200)
+        access_token = json.loads(response.data)['token']
+        headers = dict(Authorization='Bearer {}'.format(access_token))
+        response = self.client.get('/api/v2/rides/1',
+                                   content_type='application/json',
+                                   headers=headers)
+        self.assertTrue(response.status_code == 200)
+
+    def test_user_can_view_details_ofan_offer_that_doesnt_exist(self):
+        response = self.register_user()
+        self.assertTrue(response.status_code == 201)
+        response = self.login_user()
+        self.assertTrue(response.status_code == 200)
+        access_token = json.loads(response.data)['token']
+        headers = dict(Authorization='Bearer {}'.format(access_token))
+        response = self.client.get('/api/v2/rides/10000000000001',
+                                   content_type='application/json',
+                                   headers=headers)
+        self.assertTrue(response.status_code == 200)
+
     
