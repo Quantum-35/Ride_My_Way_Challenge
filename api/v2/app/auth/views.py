8aec5a91440bc4730cbd36eec9be5fa8eb522ffc
Quantum-35 <quantumcomputing35@icloud.com>
2018-06-29 05:27:49 +0300

    fix heroku

--- api/v2/app/auth/views.py
+++ api/v2/app/auth/views.py
@@ -49,7 +49,7 @@ def user_auth():
 
         if current_app.config['TESTING']:
             conn  = psycopg2.connect(host="localhost",database="test_rides", user="foo", password="bar")
-        elif current_app.config['PRODUCTION']:
+        elif current_app.config.get('production'):
             conn  = psycopg2.connect(host="ec2-54-227-247-225.compute-1.amazonaws.com",
                                     database="d59bsstdnueu2j", user="evmawfgeuwoycc", 
                                     password="51bf40de92130e038cef26d265e51c504b62bb8449d48f4794c1da44bb69a947")
@@ -63,6 +63,8 @@ def user_auth():
             new_user = User(username, email, address, password)
             new_user.register_user()
             conn.close()
+            print(current_app)
+            print(current_app.config)
             return jsonify({
                 'message': 'Signed up successfully',
                 'status': 'ok'}), 201
--- api/v2/app/create_tables.py
+++ api/v2/app/create_tables.py
@@ -11,7 +11,7 @@ def create_tables():
     # Connection to the database
     if current_app.config['TESTING']:
         conn  = psycopg2.connect(host="localhost",database="test_rides", user="foo", password="bar")
-    elif current_app.config['PRODUCTION']:
+    elif current_app.config.get('production'):
             conn  = psycopg2.connect(host="ec2-54-227-247-225.compute-1.amazonaws.com",
                                     database="d59bsstdnueu2j", user="evmawfgeuwoycc", 
                                     password="51bf40de92130e038cef26d265e51c504b62bb8449d48f4794c1da44bb69a947")     
