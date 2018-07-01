7ac9f07469cc09cbe1d58fa61f5ad015a2b79ade
Quantum-35 <quantumcomputing35@icloud.com>
2018-07-01 11:17:55 +0300

    set url_map.strict slashes to false

--- api/v2/app/app.py
+++ api/v2/app/app.py
@@ -8,6 +8,7 @@ URL_PREFIX = '/api/v2'
 def create_app(config_name):
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_object(app_config[config_name])
+    app.url_map.strict_slashes = False
 
     from app.auth.views import auth
     app.register_blueprint(auth, url_prefix=URL_PREFIX+'/auth/')
